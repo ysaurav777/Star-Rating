85648320c4b17618f237b0bd87a50a58e17647ce
Saurav Yadav <ysaurav777@gmail.com>
2024-04-13 21:29:40 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FaStar} from 'react-icons/fa';
+import './App.scss';
 
 const App = ({numStar=5}) => {
+  const [rate,setrate]=useState(0);
+  const [hov,sethov]=useState(0);
+
   function onClickStar(onindex) {
-    console.log(onindex);
+    setrate(onindex);
+    console.log(rate);
   }
 
-  function MouseMove() {
-
+  function MouseMove(onindex) {
+    sethov(onindex);
+    console.log(hov);
   }
 
-  function MouseDown() {
-
+  function MouseDown(onindex) {
+    sethov(rate);
+    console.log(hov);
   }
 
   return (
@@ -21,9 +28,10 @@ const App = ({numStar=5}) => {
           index+=1;
           return <FaStar
           key={index}
+          className={index<=(hover||rate)?'active':'inactive'}
           onClick={()=>onClickStar(index)}
-          onMouseMove={()=>MouseMove()}
-          onMouseDown={()=>MouseDown()}
+          onMouseMove={()=>MouseMove(index)}
+          onMouseDown={()=>MouseDown(index)}
           size={30}/>
         })
       }
